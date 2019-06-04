0ebfe621648f4810176dccd258df34e2e1fcded1
a-w-m <47095419+a-w-m@users.noreply.github.com>
2019-06-04 18:25:26 -0400

    0.5
    Corrected many parsing issues: line breaks for poetry and lyrics, page numbers, book chapters, and volume information showing up as quotes, and addressed broken reduce function by removing "g" flag from verifier regex.

--- quote-machine.js
+++ quote-machine.js
@@ -8,28 +8,28 @@ function main() {
 
         /* This function makes an API request and passes a random Page ID (String) of a page within "Category:People" and an array of unwanted pageIDS to the callback function (getRandomPersonPage)" (https://en.wikiquote.org/wiki/Category:People). The pages within "Category:People" are organized alphabetically by last name and contain a list of links to individual person pages */
 
-        var request = new XMLHttpRequest();
-        var peoplePagesQuery = "https://en.wikiquote.org/w/api.php?origin=*&action=query&generator=categorymembers&gcmtitle=Category:People&gcmnamespace=0&gcmlimit=500&format=json&formatversion=2"
+        let request = new XMLHttpRequest();
+        let peoplePagesQuery = "https://en.wikiquote.org/w/api.php?origin=*&action=query&generator=categorymembers&gcmtitle=Category:People&gcmnamespace=0&gcmlimit=500&format=json&formatversion=2"
 
 
         request.onreadystatechange = function() {
             if (request.readyState == 4 && request.status == 200) {
 
                 //convert JSON response to object
-                var responseJSON = (JSON.parse(request.responseText));
+                let responseJSON = (JSON.parse(request.responseText));
 
                 //random number generator from 1 to response.query.pages.length; starts at 1 because the pageID at index 0 is not an appropriate People Page
-                var randomIndex = Math.floor(Math.random() * (responseJSON.query.pages.length - 1)) + 1;
+                let randomIndex = Math.floor(Math.random() * (responseJSON.query.pages.length - 1)) + 1;
 
                 //Create an array of all the page IDs of members of "Category: People." to pass into the callback function as the filtering Array. These page ids are included in the API response to getRandomPersonPage (this function's callback function), and need to be filtered out to prevent making an API request that points back to a member of "Category:People".
 
-                var idsToFilter = responseJSON.query.pages.map(item => {
+                let idsToFilter = responseJSON.query.pages.map(item => {
                     return item.pageid
                 })
                 idsToFilter.push(94);
 
                 console.log(responseJSON["query"]["pages"][randomIndex]["pageid"].toString());
-                var pageID = responseJSON["query"]["pages"][randomIndex]["pageid"].toString();
+                let pageID = responseJSON["query"]["pages"][randomIndex]["pageid"].toString();
 
                 callback(pageID, [], idsToFilter)
 
@@ -49,9 +49,9 @@ function main() {
     function getRandomPersonPage(pageID, personPages = [], filterArray = [], callback = getQuote) {
         /* this function takes as an argument the page ID (string) of a page within "Category: People", an array that carries results for recursive calls, an array that filters unwanted page IDs returned in the JSON response and a callback function. It  a random page ID (string) of a linked person page */
 
-        var request = new XMLHttpRequest();
+        let request = new XMLHttpRequest();
         //wikimedia API requires "&continue=" within query url in order to continue the query passed the 500 element limit
-        var personPageQuery = "https://en.wikiquote.org/w/api.php?origin=*&action=query&generator=links&gplnamespace=0&gpllimit=500&prop=info&format=json&formatversion=2&pageids=" + pageID + "&continue="
+        let personPageQuery = "https://en.wikiquote.org/w/api.php?origin=*&action=query&generator=links&gplnamespace=0&gpllimit=500&prop=info&format=json&formatversion=2&pageids=" + pageID + "&continue="
 
 
 
@@ -63,14 +63,14 @@ function main() {
 
                 //convert response to object
 
-                var responseJSON = (JSON.parse(request.responseText));
+                let responseJSON = (JSON.parse(request.responseText));
 
                 //concatenate the list of results to personPages parameter
                 personPages = personPages.concat(responseJSON.query.pages)
 
                 //conditional logic and recursion to handle query results that require multiple requests using the API query continue parameter
                 if ("continue" in responseJSON) {
-                    var contPageID = pageID + responseJSON.continue.continue+"&gplcontinue=" + responseJSON.continue.gplcontinue
+                    let contPageID = pageID + responseJSON.continue.continue+"&gplcontinue=" + responseJSON.continue.gplcontinue
                     getRandomPersonPage(contPageID, personPages, filterArray, getQuote);
                 } else {
                     //Aggregated all responses from the API and now filtering out elements with missing page IDs or page IDs that lead us back up the query chain.
@@ -80,9 +80,8 @@ function main() {
                         }
                     })
                     //random number generator used to grab random element from personPages down below.
-                    var randomIndex = Math.floor(Math.random() * (personPages.length))
-                    console.log(personPages, randomIndex, personPageID)
-                    var personPageID = personPages[randomIndex]["pageid"].toString();
+                    let randomIndex = Math.floor(Math.random() * (personPages.length))
+                    let personPageID = personPages[randomIndex]["pageid"].toString();
 
                     callback(personPageID)
 
@@ -102,13 +101,13 @@ function main() {
 
     function getQuote(pageID, callback = displayQuote) {
 
-        var request = new XMLHttpRequest();
-        var quoteQuery = "https://en.wikiquote.org/w/api.php?origin=*&action=parse&format=json&prop=text%7Csections%7Cdisplaytitle%7Cparsewarnings&section=1&pageid=" + pageID
-        var quote = ""
+        let request = new XMLHttpRequest();
+        let quoteQuery = "https://en.wikiquote.org/w/api.php?origin=*&action=parse&format=json&prop=text%7Csections%7Cdisplaytitle%7Cparsewarnings&section=1&pageid=107"  
+        let quote = ""
 
 
         request.onload = function() {
-            var responseJSON = JSON.parse(request.responseText);
+            let responseJSON = JSON.parse(request.responseText);
 
             if ("error" in responseJSON) {
                 return initQuoteMachine();
@@ -118,13 +117,13 @@ function main() {
                 return initQuoteMachine();
             }
 
-            var quotes = cleanQuotes(responseJSON)
+            let quotes = cleanQuotes(responseJSON)
 
             if (quotes.length == 0) {
                 return initQuoteMachine();
             }
 
-            var randomIndex = Math.floor(Math.random() * quotes.length)
+            let randomIndex = Math.floor(Math.random() * quotes.length)
 
             callback(quotes[randomIndex], responseJSON.parse.title)
 
@@ -138,36 +137,40 @@ function main() {
     };
 
     function cleanQuotes(jsonObject) {
-        var grabDirtyQuotes = new RegExp(/<ul>(.*?)<ul>/gm);
-        var cleanUpQuotes = new RegExp(/<.*?>|\\n|\\/gm);
-        var cleanQuotes = []
-        var verifier = new RegExp(/[\[\]\^\+\{\}\=]|\(\d\)\d.\d|1.Quotes|\[edit\]/gm)
+        let grabDirtyQuotes = new RegExp(/<ul>(.*?)<ul>/gm);
+        let grabLineBreaks = new RegExp(/<br\s?\/>/gm)
+        let cleanUpQuotes = new RegExp(/<(?!\/?br(?=>|\s.*>))\/?.*?>|\\n|\\/gm);
+        let cleanQuotes = []
+        let verifierString = "[\\[\\]\\^\\+\\{\\}\\=]|\\d{4}|^\\d+$|1.Quotes|\\[edit\\]|Reffering to|p+[g]?\\.?\\s?\\d+|ch(apter)?\\.?\\s?\\d+|Book\\.?\\s?[IVXLCM]+|line+s?\\.?\\s?\\d+|Vol(ume)?\\.?|paragraph\\s?(?=\\d+|[xivcml]+)|" + jsonObject.parse.title 
+        let verifier = new RegExp(verifierString, "mi")
+        console.log((JSON.stringify(jsonObject.parse.text['*']).match(grabDirtyQuotes)))
 
-        var dirtyQuotesList = (JSON.stringify(jsonObject.parse.text['*']).match(grabDirtyQuotes))
+        try{
 
-        console.log(jsonObject)
-
-        console.log(dirtyQuotesList)
-
-        cleanQuotes = dirtyQuotesList.reduce(function(results, item) {
-
-            if (verifier.test(item.replace(cleanUpQuotes, "")) == false) {
-                results.push(item.replace(cleanUpQuotes, ""))
-                return results
-            } else {
-                return results
+        cleanQuotes = (JSON.stringify(jsonObject.parse.text['*']).match(grabDirtyQuotes)).reduce(function(results, item){
+            if (verifier.test(item.replace(grabLineBreaks, "/ ").replace(cleanUpQuotes, "")) !=true){
+                results.push(item.replace(grabLineBreaks, "<br>").replace(cleanUpQuotes, ""))
             }
-        }, []);
+            return results
 
+        }, [])
+    }
+        
+        catch(err){
+            initQuoteMachine();
+        }
+    
         console.log(cleanQuotes)
+        console.log(grabLineBreaks.test("<ul><li>Verse sweetens toil, however rude the sound;<br />She feels no biting pang the while she sings;<br />Nor, as she turns the giddy wheel around,<br />Revolves the sad vicissitudes of things.\n<ul>"))
         return cleanQuotes
+        
 
 
     };
 
 
 
-    function displayQuote(quote, author, ) {
+    function displayQuote(quote, author) {
 
         document.getElementById("text").innerHTML = quote;
         document.getElementById("author").innerHTML = author;
@@ -175,40 +178,58 @@ function main() {
 
 
         document.getElementById('new-quote').onclick = function() {
-
+          
             if (displayQuote.previous == undefined) {
-                displayQuote.previous = []
+                displayQuote.previous = [{
+                    "quote": quote,
+                    "author": author
+            
+                }];
+
+            }else{
+                displayQuote.previous.push({
+                    "quote": quote,
+                    "author": author
+            
+                })
+
+               
             }
 
-            displayQuote.previous.push({
-                "quote": quote,
-                "author": author
-            })
             displayQuote.lastIndex = displayQuote.previous.length - 1
-            initQuoteMachine();
+            initQuoteMachine()
+
+            
+         
+            
         }
 
         document.getElementById("prev-quote").onclick = function() {
-            console.log(displayQuote.previous, displayQuote.lastIndex)
-            document.getElementById("text").innerHTML = displayQuote.previous[displayQuote.lastIndex].quote
+            document.getElementById("text").innerHTML = displayQuote.previous[displayQuote.lastIndex]["quote"]
             document.getElementById("author").innerHTML = displayQuote.previous[displayQuote.lastIndex]["author"]
-            displayQuote.lastIndex--;
-        }
+            displayQuote.lastIndex--;        
+        
+    }
+    
+        document.getElementById("tweet-link").setAttribute('href', "http://twitter.com/intent/tweet?text=" + document.getElementById("text").innerHTML)
+
+        document.getElementById("tumblr-link").setAttribute('href', "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&caption=" + encodeURI(document.getElementById("author").innerHTML) + "&content="+ encodeURI(document.getElementById("text").innerHTML) +"&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button&posttype=quote")
+
+
 
     }
-
 
     initQuoteMachine();
 }
 
 
-
-
 main();
 
+//Issue:
 
 //fix hawthorne numbers
 //fix poetry (de kok) line breaks (jacques prevert) {replace <br> with /}
 //quote of the day showing up
 //stub pages Qin Shi Huang
 //page numbers stephen covey
+//dots Michael Friendly
